1e095f0431ce443d36657ba4a3bd4408a21ac7d3
jon <jrinciari@or-specialties.com>
2017-12-14 00:39:11 +0000

    updated authorization to use github strategy

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -12,14 +12,14 @@ router.get('/logout', (req, res) => {
     res.redirect('/');
 });
 
-// auth with google+
-router.get('/google', passport.authenticate('google', {
+// auth with github
+router.get('/github', passport.authenticate('google', {
     scope: ['profile']
 }));
 
 // callback route for google to redirect to
 // hand control to passport to use code to grab profile info
-router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
+router.get('/github/redirect', passport.authenticate('github'), (req, res) => {
     res.send(req.user);
     
 });
