6d0586d6584ba7792aa52b2734899a7a226b7d76
jon <jrinciari@or-specialties.com>
2017-12-15 21:59:43 +0000

    add event listener for attendance button, and set up html to be appended for each search result

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,17 +1,40 @@
 $(document).ready(function(){
-    $('button').click(() =>{
+    $('.btn').click(() =>{
+        $('.resultContainer').empty();
         var search = $('input').val();
+        console.log(search)
         $.ajax({
             type: 'GET',
             url: '/search',
             data: {search: search}
         }).then( (data) => {
             data.forEach((data) =>{
-                $('ul').append(`
-                <li> ${data.name} </li>
-                <li> ${data.address} </li>
-                `)
-            })
-        })
-    })
+                
+                var el = `<li class="results> 
+                    <a href='${data.link}'>
+                        <div class="title> 
+                            <h3> ${data.name} </h3>
+                            <h5> ${data.address} </h5>
+                        </div>
+                        <button class="attendence"> 
+                            Going? <span> ${data.attending} </span> 
+                        </button>
+                    </a>
+                </li>
+                `
+    $('ul').append(el)
+    
 })
+})
+    
+   
+})
+
+    
+
+  });
+  
+  $('ul').on('click', '.attendence', function(){
+      console.log($(this).children('span').text())
+  })
+ 
